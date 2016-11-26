e6086d86988771a47da80a25cfeeaea94a581f70
chaozhou <1161192890@qq.com>
2016-11-27 00:55:45 +0800

    basic

--- tushareTools/get_stock_basics.py
+++ tushareTools/get_stock_basics.py
@@ -12,11 +12,38 @@ import tushare as ts
 import os
 import pandas as pd
 
+#股票名称、代码
+def get_stock():
+    g = ts.get_stock_basics()
+    t=g['name']
+    datapath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    filename = os.path.join(datapath, 'data', 'code.cvs')
+    t.to_csv(filename)
+    return t
+
 def get_stock_basics():
     g=ts.get_stock_basics()
-    datapath = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + r'\data\cvs'
-    g.to_csv(datapath)
+    datapath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    filename = os.path.join(datapath,'data','basic.cvs')
+    filetrade = os.path.join(datapath, 'data', 'trade.cvs')
+    print filename
+    g = g[['name','reservedPerShare','bvps','perundp']]
+    g.to_csv(filename)
+
+    ttrade = ts.get_today_all()
+    ttrade = ttrade[['code','trade']]
+    ttrade.to_csv(filetrade)
+    g = pd.read_csv(filename)
+
+    ttrade = pd.read_csv(filetrade)
+    ttrade = ttrade[['code','trade']]
+    g = pd.merge(g,ttrade,how = 'outer',on='code')
+    g['total'] = g['reservedPerShare'] + g['bvps'] + g['perundp']
+    g['end'] = g['trade'] - g['total']
+    g = g.sort(['end'])
+    g.to_csv(filename)
+
     return g
 
 if __name__ == '__main__':
-    get_stock_basics()
+    get_stock()
